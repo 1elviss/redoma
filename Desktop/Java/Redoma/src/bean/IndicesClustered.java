2b43eccb9807f153973df9615e0c181a452318c0
Aldambs <36906787+Aldambs@users.noreply.github.com>
2018-08-28 21:05:22 -0300

    Update and rename IndicesCluster.java to IndicesClustered.java

---
+++ Desktop/Java/Redoma/src/bean/IndicesClustered.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean;
+
+/**
+ *
+ * @author aldam
+ */
+public class IndicesClustered {
+    private String nomeTabela;
+    private String nomeIndice;
+    private String descricaoIndice;
+    private int fragmentacao;
+
+    public String getNomeTabela() {
+        return nomeTabela;
+    }
+
+    public void setNomeTabela(String nomeTabela) {
+        this.nomeTabela = nomeTabela;
+    }
+
+    public String getNomeIndice() {
+        return nomeIndice;
+    }
+
+    public void setNomeIndice(String nomeIndice) {
+        this.nomeIndice = nomeIndice;
+    }
+
+    public String getDescricaoIndice() {
+        return descricaoIndice;
+    }
+
+    public void setDescricaoIndice(String descricaoIndice) {
+        this.descricaoIndice = descricaoIndice;
+    }
+
+    public int getFragmentacao() {
+        return fragmentacao;
+    }
+
+    public void setFragmentacao(int fragmentacao) {
+        this.fragmentacao = fragmentacao;
+    }
+    
+    public String nomedoSelect() {
+        String nomedoSelect = String.format("***Índices Clustered***%n");
+        return nomedoSelect;
+    }
+
+    public String cabecalho() {
+        String cabecalhoTabela = String.format("|%-40s|%-120s|%-20s|%-20s|\n",
+                "nomeTabela", "nomeIndice", "descricaoIndice", "fragmentacao");
+        return cabecalhoTabela;
+    }
+
+    @Override
+    public String toString() {
+        String corpoTabela = String.format("|%-40s|%-120s|%-20s|%-20s|\n",
+                nomeTabela,nomeIndice, descricaoIndice, fragmentacao);
+        return corpoTabela;
+    }
+}
