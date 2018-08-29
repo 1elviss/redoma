b29899d89cc2951cb02e34698044fec504dce114
Aldambs <36906787+Aldambs@users.noreply.github.com>
2018-08-28 21:10:00 -0300

    Update Tela_Resumo.java

--- Desktop/Java/Redoma/src/view/Tela_Resumo.java
+++ Desktop/Java/Redoma/src/view/Tela_Resumo.java
@@ -1,7 +1,12 @@
 package view;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import util.Arquivo;
 import util.ConnectionFactory;
